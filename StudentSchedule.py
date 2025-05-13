c11806fbf187b68cdf4089e0b268e9bf0f1cf3ea
DaichiBT <144473574+DaichiBT@users.noreply.github.com>
2025-05-13 19:29:46 -0400

    Add files via upload

--- StudentSchedule.py
+++ StudentSchedule.py
@@ -1,32 +1,42 @@
 import mysql.connector
 
 def get_database_connection():
-   connection = mysql.connector.connect(
-      user='daichit',
-      password='230958233',
-      host='10.8.37.226',
-      database='daichit_db'
-   )
-   return connection
+    connection = mysql.connector.connect(
+        user='daichit',
+        password='230958233',
+        host='10.8.37.226',
+        database='daichit_db'
+    )
+    return connection
 
-cursor = connection.cursor()
+def execute_statement(connection, statement):
+    cursor = connection.cursor()
+    cursor.execute(statement)
+    results = []
 
+    for row in cursor:
+        results.append(row)
+
+    cursor.close()
+    connection.close()
+    return results
+
+def get_student_schedule(student_id):
+    statement = f"CALL Get_Student_Schedule({student_id});"
+    return execute_statement(get_database_connection(), statement)
 
 student_id = input("Enter a student ID: ")
+results = get_student_schedule(student_id)
 
+for result in results:
+    period = result[0]
+    class_name = result[1]
+    room = result[2]
+    teacher_first = result[3]
+    teacher_last = result[4]
 
-query = f"CALL Get_Student_Schedule({student_id});"
-cursor.execute(query)
-
-
-print()
-for row in cursor:
-   print(f"Period: {row[0]}")
-   print(f"Course: {row[1]}")
-   print(f"Room: {row[2]}")
-   print(f"Teacher: {row[3]} {row[4]}")
-   print()
-
-
-cursor.close()
-connection.close()
+    print(f"Period: {period}")
+    print(f"Course: {class_name}")
+    print(f"Room: {room}")
+    print(f"Teacher: {teacher_first} {teacher_last}")
+    print()
